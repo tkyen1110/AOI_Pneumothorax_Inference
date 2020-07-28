2be2ead42b80a5911bac2f9a5b11da35af474507
Tk.Yen <tkyen1110@gmail.com>
2020-07-28 13:26:16 +0800

    Revise auto_curl_test.sh

--- pneu_online/auto_curl_test.sh
+++ pneu_online/auto_curl_test.sh
@@ -5,10 +5,14 @@ ls_result=$(echo ${ls_result})
 IFS=" " read -a images <<< $ls_result
 
 echo "Total ${#images[@]} images"
-for (( i=0; i<${#images[@]}; i+=1 ))
+
+while true
 do
-  echo $i, ${images[$i]}
-  cmd="curl -X POST http://203.145.222.39:5050/ADV_pneu --header \"Content-Type: application/json\" --data '{\"dcm\": \"${images[$i]}\"}'"
-  eval $cmd
-  echo ""
+  for (( i=0; i<${#images[@]}; i+=1 ))
+  do
+    echo $i, ${images[$i]}
+    cmd="curl -X POST http://103.124.73.114:8082/ADV_pneu --header \"Content-Type: application/json\" --data '{\"dcm\": \"${images[$i]}\"}'"
+    eval $cmd
+    echo ""
+  done
 done
--- pneu_online/auto_curl_test_reverse.sh
+++ pneu_online/auto_curl_test_reverse.sh
@@ -5,12 +5,16 @@ ls_result=$(echo ${ls_result})
 IFS=" " read -a images <<< $ls_result
 
 echo "Total ${#images[@]} images"
-for (( i=0; i<${#images[@]}; i+=1 ))
+
+while true
 do
-  total=${#images[@]}
-  (( j = total - i - 1))
-  echo $j, ${images[$j]}
-  cmd="curl -X POST http://203.145.222.39:5050/ADV_pneu --header \"Content-Type: application/json\" --data '{\"dcm\": \"${images[$j]}\"}'"
-  eval $cmd
-  echo ""
+  for (( i=0; i<${#images[@]}; i+=1 ))
+  do
+    total=${#images[@]}
+    (( j = total - i - 1))
+    echo $j, ${images[$j]}
+    cmd="curl -X POST http://103.124.73.114:8082/ADV_pneu --header \"Content-Type: application/json\" --data '{\"dcm\": \"${images[$j]}\"}'"
+    eval $cmd
+    echo ""
+  done
 done
