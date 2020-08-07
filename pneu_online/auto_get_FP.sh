be70a4c97658d8b2c965745b91807cd951dc3e78
Tk.Yen <tkyen1110@gmail.com>
2020-08-07 11:19:31 +0800

    Revise auto_get_FP.sh

--- pneu_online/auto_get_FP.sh
+++ pneu_online/auto_get_FP.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-echo "Please input no_pneu log file name:"
+echo "Please input no_pneu log file name: (ex: 775_no_pneu.txt)"
 read logfile
 
-echo "Please input no_pneu folder name:"
+echo "Please input no_pneu folder name: (ex: 775_no_pneu)"
 read no_pneu_folder
 
 IFS="." read -a filename <<< $logfile
-fp_folder="${filename[0]}_fp"
-if [ ! -d $fp_folder ]
+fp_png_folder="${filename[0]}_fp_png"
+fp_vis_folder="${filename[0]}_fp_vis"
+
+if [ ! -d $fp_png_folder ]
 then
-  mkdir $fp_folder
+  mkdir $fp_png_folder
+fi
+
+if [ ! -d $fp_vis_folder ]
+then
+  mkdir $fp_vis_folder
 fi
 
 total=0
@@ -28,11 +35,19 @@ do
   then
     ((pneu+=1))
     line=`echo $line | sed 's/{//g' | sed 's/}//g' | sed 's/'\"'//g'`
-    IFS="," read -a element <<< $line
-    IFS="/" read -a element <<< ${element[3]}
-    IFS="_" read -a element <<< ${element[-1]}
-    file="${element[0]}.png"
-    cp $no_pneu_folder/$file $fp_folder
+
+    IFS="," read -a elements <<< $line
+
+    # I00291013740.png
+    IFS="/" read -a element <<< ${elements[4]}
+    file_png=${element[-1]}
+
+    # I00291013740_1_0.729.png
+    IFS="/" read -a element <<< ${elements[3]}
+    file_vis=${element[-3]}/${element[-2]}/${element[-1]}
+
+    cp $no_pneu_folder/$file_png $fp_png_folder
+    cp $file_vis $fp_vis_folder
   else
     ((error+=1))
   fi
